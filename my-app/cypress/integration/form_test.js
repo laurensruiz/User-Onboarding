6933599b00af01dac9be94f49d459c4941743fec
laurensruiz <laurenruiz97@yahoo.com>
2022-03-31 13:23:28 -0400

    TOS checkbox can be checked

--- my-app/cypress/integration/form_test.js
+++ my-app/cypress/integration/form_test.js
@@ -1,6 +1,6 @@
 //write tests here
-beforeEach(() => {
-    describe("Form App", () => {
+describe("Form App", () => {
+    beforeEach(() => {
         /*
         *Each Test need a fresh state so we must refresh page
         */
@@ -12,6 +12,7 @@ beforeEach(() => {
     const usernameInput = () => cy.get("input[name=username]");
     const emailInput = () => cy.get("input[name=email]");
     const passwordInput = () => cy.get("input[name=password]");
+    const tosInput = () => cy.get("input[type=checkbox]").check();
     const fooBarInput = () => cy.get("input[name=foobar]");
     const submitBtn = () => cy.get("input[id=submitBtn");
 
@@ -22,22 +23,52 @@ beforeEach(() => {
         expect(1+2).to.equal(3);
         expect(2+2).not.to.equal(5);
         expect({}).not.to.equal({}); // === : each object references to a space in memory, but objects save in different spaces in memory
-        expect({}).to.equal({}); // == ; same thing but not exactly
+        expect({}).to.eql({}); // == ; same thing but not exactly note syntax
     })
 
     it("The proper elements are showing", () => {
         usernameInput().should("exist");
         emailInput().should("exist");
         passwordInput().should("exist");
+        tosInput().should("exist");
         fooBarInput().should("not.exist");
         submitBtn().should("exist");
+    })
+
+    describe("Filling out the inputs and cancelling", () => {
+        // You can use (optional) "describe" blocks to organize
+        // our tests and group them under a top level heading
+        it("can navigate to the site", () => {
+          cy.url().should("include", "localhost");
+        })
+
+        it("Can type in the username, password and email", () => {
+            usernameInput()
+              .should("have.value", "")
+              .type("laurensruiz")
+              .should("have.value", "laurensruiz");
+            
+            emailInput()
+              .should("have.value", "")
+              .type("sample_email@gmail.com")
+              .should("have.value", "sample_email@gmail.com");
+            
+            passwordInput()
+              .should("have.value", "")
+              .type("123456")
+              .should("have.value", "123456");
+        })
 
     })
 
-
-
-
-
+    describe("TOS can be checked", () => {
+        it("can be checked", () => {
+            tosInput()
+            .should('be.visible') 
+            .check({ force: true }) // checks the box
+            .should('be.checked')
+            })
+        })
 
 
 
